5b1e9dff28f142f08bbd80f0c14b9bebd5845577
musa <mosesbwire91@gmail.com>
2021-02-14 14:10:13 +0300

    Implement basic login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     officer = RelationshipOfficer.find_by(email: params[:email].downcase)
     if officer && officer.authenticate(params[:password])
-      #go to user profile
+      log_in officer
+      redirect_to officer
     else
       flash.now[:danger] = "Invalid email/ Password"
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+
+    def log_in(user)
+        session[:relationship_officer_id] = user.id
+    end
+
+    def current_user
+        @current_user ||= RelationshipOfficer.find_by(id: session[:relationship_officer_id])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
 end
