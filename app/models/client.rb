380080d39fc1253f3f0eb05fe8d853956783fda4
musa <mosesbwire91@gmail.com>
2021-02-14 18:36:38 +0300

    Add static pages controller

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,7 @@ class Client < ApplicationRecord
     belongs_to :relationship_officer
     validates :name, presence: true, length: {minimum: 6, maximum: 50}
     validates :phone_no, presence: true, length: {is: 10}
-    validate :valid_phone #fix using regex
+    validate :valid_phone  #fix using regex
 
     def valid_phone
         errors.add(:phone_no, "Invalid format,number should start with zero") if phone_no.split("").first != "0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/signup', to: 'relationship_officers#new'
