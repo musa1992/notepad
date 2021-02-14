5d58ca7bce972f07a4c88b56156f42db06793744
musa <mosesbwire91@gmail.com>
2021-02-14 12:49:31 +0300

    Test for invalid logins plus add flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    officer = RelationshipOfficer.find_by(email: params[:email].downcase)
+    if officer && officer.authenticate(params[:password])
+      #go to user profile
+    else
+      flash.now[:danger] = "Invalid email/ Password"
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   get '/signup', to: 'relationship_officers#new'
   post '/signup', to: 'relationship_officers#create'
   resources :relationship_officers
