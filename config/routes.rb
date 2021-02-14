b11020cb03649b24c46e4c17709c0d36f2fdd202
musa <mosesbwire91@gmail.com>
2021-02-14 12:16:34 +0300

    Add sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
   get '/signup', to: 'relationship_officers#new'
   post '/signup', to: 'relationship_officers#create'
   resources :relationship_officers
