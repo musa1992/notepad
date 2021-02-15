416a03b0c63d114fb4551332a76cb8990b60c264
musa <mosesbwire91@gmail.com>
2021-02-15 22:16:55 +0300

    update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'relationship_officers#new'
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
