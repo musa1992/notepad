1b1806c08d37221ef785bd863703247398c226ab
musa <mosesbwire91@gmail.com>
2021-02-17 21:52:01 +0300

    Calculate end date , retrieve due loans for that day only

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,11 +1,17 @@
 class Client < ApplicationRecord
+    before_save {self.end_date = due_date + ((loan_duration * 7).days)}
     belongs_to :relationship_officer
     validates :name, presence: true, length: {minimum: 6, maximum: 50}
     validates :phone_no, presence: true, length: {is: 10}
     validate :valid_phone  #fix using regex
     validates :relationship_officer_id, presence: true
 
-    def valid_phone
-        errors.add(:phone_no, "Invalid format,number should start with zero") if phone_no.split("").first != "0"
-    end
+   
+    private
+
+     def valid_phone
+         errors.add(:phone_no, "Invalid format,number should start with zero") if phone_no.split("").first != "0"
+     end
+
+
 end
--- app/models/relationship_officer.rb
+++ app/models/relationship_officer.rb
@@ -1,4 +1,5 @@
 class RelationshipOfficer < ApplicationRecord
+    require 'date'
     before_save {self.email = email.downcase}
     validates :name, presence: true, length: {minimum: 6, maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -8,4 +9,12 @@ class RelationshipOfficer < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}
     has_many :clients
+
+    def loans_due
+        @dues = []
+        self.clients.each do |client|
+            @dues << client if client.due_date == Date.today
+        end
+        @dues
+    end
 end
