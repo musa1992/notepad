01222249a658b047754ddb0b97b4cfd5d6b6706d
musa <mosesbwire91@gmail.com>
2021-02-22 22:01:41 +0300

    Add sidekiq-scheduler background job update due dates

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @current_user = current_user
+   
   end
 
   def about
---
+++ app/workers/update_due_date_worker.rb
@@ -0,0 +1,15 @@
+class UpdateDueDateWorker
+  require 'sidekiq-scheduler'
+  include Sidekiq::Worker
+  #sidekiq_options retry: false
+
+  def perform
+    clients = Client.where('due_date < ?', Date.current)
+    clients.each do |client|
+      date = client.due_date + (7.days)
+      client.update_columns(due_date: date)
+    end
+  end
+
+  UpdateDueDateWorker.perform_async
+end
