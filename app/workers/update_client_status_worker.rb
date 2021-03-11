eaee7a74ca54a20659bf2091ca34929ba731eb18
musa <mosesbwire91@gmail.com>
2021-03-11 23:08:58 +0300

    Edit workers

--- app/workers/update_client_status_worker.rb
+++ app/workers/update_client_status_worker.rb
@@ -1,12 +1,13 @@
 class UpdateClientStatusWorker
     require 'sidekiq-scheduler'
     include Sidekiq::Worker
+    sidekiq_options retry: false
 
-    def perform
-        clients = Client.where ('end_date < ?', Date.current)
-        clients.each do |client|
-            client.update_columns(end_date: 'Inactive')
-        end
-    end
-    UpdateClientStatusWorker.perform_async
+    # def perform
+    #     clients = Client.where ('end_date < ?', Date.current)
+    #     clients.each do |client|
+    #         client.update_columns(end_date: 'Inactive')
+    #     end
+    # end
+    # UpdateClientStatusWorker.perform_async
 end
--- app/workers/update_due_date_worker.rb
+++ app/workers/update_due_date_worker.rb
@@ -1,15 +1,15 @@
 class UpdateDueDateWorker
   require 'sidekiq-scheduler'
   include Sidekiq::Worker
-  #sidekiq_options retry: false
+  sidekiq_options retry: false
 
-  def perform
-    clients = Client.where('due_date < ?', Date.current)
-    clients.each do |client|
-      date = client.due_date + (7.days)
-      client.update_columns(due_date: date)
-    end
-  end
+  # def perform
+  #   clients = Client.where('due_date < ?', Date.current)
+  #   clients.each do |client|
+  #     date = client.due_date + (7.days)
+  #     client.update_columns(due_date: date)
+  #   end
+  # end
 
-  UpdateDueDateWorker.perform_async
+  # UpdateDueDateWorker.perform_async
 end
