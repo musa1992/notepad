1013153efb47cfb31fc721e46c5b61b52bd9fb43
musa <mosesbwire91@gmail.com>
2021-03-26 21:14:12 +0300

    add validations to loan model

--- app/models/short_loan.rb
+++ app/models/short_loan.rb
@@ -1,2 +1,18 @@
 class ShortLoan < ApplicationRecord
+    validates :loan_amount, presence: true, length: { in: 4..5 }, numericality: true
+    validates :loan_duration, presence: true
+    validates :instalment_amount, presence: true, length: { in: 3..5}, numericality: true
+
+
+
+
+    private 
+
+        def initial_loan_balance
+            self.instalment_amount * self.loan_duration
+        end
+
+        def generate_loan_schedule
+            
+        end
 end
